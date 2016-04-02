bedaad0ba1c3e945a73edd5122a0cb9d19dae12f
boringtao <boringtaoxh@gmail.com>
2016-04-02 02:20:16 +0100

    localstorage city

--- public/app/controllers/allianceController.js
+++ public/app/controllers/allianceController.js
@@ -1,27 +1,42 @@
 angular
-  .module('hshs').controller('allianceController', ['$rootScope', '$scope', '$routeParams', '$sce', 'allianceService', 'clubService', 'cityService', function ($rootScope, $scope, $routeParams, $sce, allianceService, clubService, cityService) {
+  .module('hshs').controller('allianceController', ['$rootScope', '$scope', '$routeParams', '$sce', '$localStorage', 'allianceService', 'clubService', 'cityService', function ($rootScope, $scope, $routeParams, $sce, $localStorage, allianceService, clubService, cityService) {
+
+    var allianceId, clubId, cities;
 
     allianceService.get().then(function(data) {
       $scope.alliances = data.data.result;
     });
 
     if ($routeParams.allianceId) {
-      var allianceId = $routeParams.allianceId;
+      allianceId = $routeParams.allianceId;
 
       cityService.get(allianceId).then(function(data) {
-        $scope.city = data.data['0']['name'];
-        clubService.get(allianceId).then(function(data) {
-          $scope.clubs = data.data.result;
-        });
+        if (!_.isEmpty(data.data)) {
+          $scope.cityName = data.data['0']['name'];
+        }
+      });
+
+      clubService.get(allianceId).then(function(data) {
+        $scope.clubs = data.data.result;
       });
     }
 
     if ($routeParams.clubId) {
-      var clubId = $routeParams.clubId;
+      clubId = $routeParams.clubId;
 
       clubService.getDetail(clubId).then(function(data) {
         $scope.clubDetail = data.data;
         $scope.clubDescription = $sce.trustAsHtml(data.data.description);
+
+        if ($localStorage.vmaCity === null) {
+          cityService.getAll().then(function(data) {
+            cities = data.data;
+            $scope.cityName = cityService.getName(cities);
+          });
+        } else {
+          cities = $localStorage.vmaCity;
+          $scope.cityName = cityService.getName(cities, data.data.cityId);
+        }
       });
     }
 
--- public/app/services/cityService.js
+++ public/app/services/cityService.js
@@ -1,5 +1,5 @@
 angular
-  .module('hshs').factory('cityService', ['$rootScope', '$http', 'toaster', 'baseUrl', function ($rootScope, $http, toaster, baseUrl) {
+  .module('hshs').factory('cityService', ['$rootScope', '$http', '$localStorage', 'toaster', 'baseUrl', function ($rootScope, $http, $localStorage, toaster, baseUrl) {
     var promise = {};
 
     output = {
@@ -14,6 +14,23 @@ angular
           console.log(status);
         });
         return promise;
+      },
+      getAll: function () {
+        promise = $http({
+          method: 'GET',
+          url: baseUrl + 'regions/list',
+          params: { },
+        }).success(function (response) {
+          $localStorage.vmaCity = response;
+          return response;
+        }).error(function (data, status) {
+          console.log(status);
+        });
+        return promise;
+      },
+      getName: function (cities, cityId) {
+        var city = _.find(cities, {"id": cityId});
+        return city.name;
       }
     };
 

5d885a41e1e4bcbc7fefb259659c425c3d761a5f
Tao <Tao@macs-Mac-mini.local>
2016-04-04 01:13:35 +0800

    clubs refactoring

--- public/app/controllers/clubsController.js
+++ public/app/controllers/clubsController.js
@@ -2,25 +2,22 @@ angular
   .module('hshs').controller('clubsController', ['$rootScope', '$scope', '$routeParams', '$sce', '$localStorage', 'allianceService', 'clubService', 'cityService', function ($rootScope, $scope, $routeParams, $sce, $localStorage, allianceService, clubService, cityService) {
 
     var allianceId;
+    allianceId = parseInt($routeParams.allianceId);
+    $scope.allianceId = allianceId;
 
-    if ($routeParams.allianceId) {
-      allianceId = parseInt($routeParams.allianceId);
-      $scope.allianceId = allianceId;
-
-      allianceService.getAll().then(function(data) {
-        $scope.allianceName = allianceService.getName(data.data.result, allianceId);
-        $localStorage.vmaAllianceName = $scope.allianceName;
-      }).then(function(){
-        cityService.get(allianceId).then(function(data) {
-          if (!_.isEmpty(data.data)) {
-            $scope.cityName = data.data['0']['name'];
-          }
-        });
-
-        clubService.getAll(allianceId).then(function(data) {
-          $scope.clubs = data.data.result;
-        })
+    allianceService.getAll().then(function(data) {
+      $scope.allianceName = allianceService.getName(data.data.result, allianceId);
+      $localStorage.vmaAllianceName = $scope.allianceName;
+    }).then(function(){
+      cityService.get(allianceId).then(function(data) {
+        if (!_.isEmpty(data.data)) {
+          $scope.cityName = data.data['0']['name'];
+        }
       });
-    }
+      clubService.getAll(allianceId).then(function(data) {
+        $scope.clubs = data.data.result;
+      })
+    });
+
 
   }]);
